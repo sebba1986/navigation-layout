ebd379fc62d2a01f20431f353ecb094429f309bc
sebba1986 <70055653+sebba1986@users.noreply.github.com>
2020-10-27 22:35:19 +0200

    modified main.css and main.js

--- resources/main.js
+++ resources/main.js
@@ -24,20 +24,44 @@ document.addEventListener("DOMContentLoaded", function(event) {
             }
             document.getElementsByClassName(dataContent)[0].classList.remove("hidden");
             let imgLogo = document.getElementById("imgLogo");
+            let boundaryMenu = document.getElementsByClassName("boundary-menu");
             // este mai recomandat sa implementezi un switch aici, decat atatea if..else-uri
-            if(dataContent == "your-orders-wrapper") {
-                imgLogo.src = "../navigation-layout/assets/logo-vip.png";
-            } else if(dataContent == "reward-status-wrapper") {
-                imgLogo.src = "../navigation-layout/assets/logo-gold.png";
-            } else if(dataContent == "account-settings-wrapper") {
-                imgLogo.src = "../navigation-layout/assets/logo-elite.png";
-            } else if(dataContent == "payment-methods-wrapper") {
-                imgLogo.src = "../navigation-layout/assets/logo-member.png";
+            // if(dataContent == "your-orders-wrapper") {
+            //     imgLogo.src = "../navigation-layout/assets/logo-vip.png";
+            // } else if(dataContent == "reward-status-wrapper") {
+            //     imgLogo.src = "../navigation-layout/assets/logo-gold.png";
+            // } else if(dataContent == "account-settings-wrapper") {
+            //     imgLogo.src = "../navigation-layout/assets/logo-elite.png";
+            // } else if(dataContent == "payment-methods-wrapper") {
+            //     imgLogo.src = "../navigation-layout/assets/logo-member.png";
+            // }
+            switch(dataContent) {
+                case "your-orders-wrapper": 
+                    imgLogo.src = "../navigation-layout/assets/logo-vip.png";
+                    break;
+                case "reward-status-wrapper":
+                    imgLogo.src = "../navigation-layout/assets/logo-gold.png";
+                    break;
+                case "account-settings-wrapper":
+                    imgLogo.src = "../navigation-layout/assets/logo-elite.png";
+                    break;
+                case "payment-methods-wrapper":
+                    imgLogo.src = "../navigation-layout/assets/logo-member.png";                
+            }
+            if(dataContent=="your-orders-wrapper") {
+                Array.from(boundaryMenu).forEach(function(item){
+                    item.classList.add("hidden");
+                })
+            } else {
+                Array.from(boundaryMenu).forEach(function(item){
+                    item.classList.remove("hidden");
+                })
             }
         })
     }
     const orderDetails = document.getElementById("orderDetails");
     const orderDetailsBtn = document.getElementById("orderDetailsBtn");
+    let boundaryMenu = document.getElementsByClassName("boundary-menu");
     orderDetailsBtn.addEventListener("click", function() {
         const contentElements = document.getElementsByClassName("content");
             for(let k=0; k < contentElements.length; k++) {
@@ -48,5 +72,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 }
             }
         orderDetails.classList.remove("hidden");
+        Array.from(boundaryMenu).forEach(function(item){
+            item.classList.remove("hidden");
+        })
     })
 })
