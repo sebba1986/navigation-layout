7ecaf66c1941678eafee05b6605a17480639b16f
sebba1986 <70055653+sebba1986@users.noreply.github.com>
2020-11-03 14:51:02 +0200

    modified main.js

--- resources/main.js
+++ resources/main.js
@@ -1,79 +1,49 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    const menuItems = document.getElementsByClassName("menu-item");
-    for(let i=0; i < menuItems.length; i++) {
-        const menuItem = menuItems[i];
-        menuItem.addEventListener("click", function() {
-            for(let j=0; j < menuItems.length; j++) {
-                if(menuItems[j].classList.contains("underline")) {
-                    menuItems[j].classList.remove("underline");
-                } else {
-                    continue;
-                }
-            }
-            menuItem.classList.add("underline");
 
+$(function(){
+    let menuItems = $(".menu-item")
+    let contentElements = $(".content")
+    menuItems.on("click", function() {
+        menuItems.removeClass("underline");
+        $(this).addClass("underline");
+        let dataContent = $(this).data("content")
+        console.log(dataContent)
+        contentElements.addClass("hidden");
+        // contentElements.each(function(index, elem){
+        //     if($(elem).hasClass(dataContent)){
+        //         $(elem).removeClass("hidden");
+        //     }
+        //                OR USE
+        //
+        // let classDataContent = "." + dataContent
+        // contentElements.filter(classDataContent).removeClass("hidden");
+        //
+        //              OR THE BACKTICK :
+        contentElements.filter(`.${dataContent}`).removeClass("hidden");
 
-            const dataContent = menuItem.dataset.content;
-            const contentElements = document.getElementsByClassName("content");
-            for(let k=0; k < contentElements.length; k++) {
-                if(contentElements[k].classList.contains("hidden")) {
-                    continue;
-                } else {
-                    contentElements[k].classList.add("hidden");
-                }
-            }
-            document.getElementsByClassName(dataContent)[0].classList.remove("hidden");
-            let imgLogo = document.getElementById("imgLogo");
-            let boundaryMenu = document.getElementsByClassName("boundary-menu");
-            // este mai recomandat sa implementezi un switch aici, decat atatea if..else-uri
-            // if(dataContent == "your-orders-wrapper") {
-            //     imgLogo.src = "../navigation-layout/assets/logo-vip.png";
-            // } else if(dataContent == "reward-status-wrapper") {
-            //     imgLogo.src = "../navigation-layout/assets/logo-gold.png";
-            // } else if(dataContent == "account-settings-wrapper") {
-            //     imgLogo.src = "../navigation-layout/assets/logo-elite.png";
-            // } else if(dataContent == "payment-methods-wrapper") {
-            //     imgLogo.src = "../navigation-layout/assets/logo-member.png";
-            // }
-            switch(dataContent) {
-                case "your-orders-wrapper": 
-                    imgLogo.src = "../navigation-layout/assets/logo-vip.png";
-                    break;
-                case "reward-status-wrapper":
-                    imgLogo.src = "../navigation-layout/assets/logo-gold.png";
-                    break;
-                case "account-settings-wrapper":
-                    imgLogo.src = "../navigation-layout/assets/logo-elite.png";
-                    break;
-                case "payment-methods-wrapper":
-                    imgLogo.src = "../navigation-layout/assets/logo-member.png";                
-            }
-            if(dataContent=="your-orders-wrapper") {
-                Array.from(boundaryMenu).forEach(function(item){
-                    item.classList.add("hidden");
-                })
-            } else {
-                Array.from(boundaryMenu).forEach(function(item){
-                    item.classList.remove("hidden");
-                })
-            }
-        })
-    }
-    const orderDetails = document.getElementById("orderDetails");
-    const orderDetailsBtn = document.getElementById("orderDetailsBtn");
-    let boundaryMenu = document.getElementsByClassName("boundary-menu");
-    orderDetailsBtn.addEventListener("click", function() {
-        const contentElements = document.getElementsByClassName("content");
-            for(let k=0; k < contentElements.length; k++) {
-                if(contentElements[k].classList.contains("hidden")) {
-                    continue;
-                } else {
-                    contentElements[k].classList.add("hidden");
-                }
-            }
-        orderDetails.classList.remove("hidden");
-        Array.from(boundaryMenu).forEach(function(item){
-            item.classList.remove("hidden");
-        })
+        if(dataContent == "your-orders-wrapper"){
+        $(".boundary-menu").addClass("hidden");
+        } else {
+        $(".boundary-menu").removeClass("hidden");
+        }
+
+        switch(dataContent) {
+            case "your-orders-wrapper":
+                $("#imgLogo").attr("src", "../navigation-layout/assets/logo-vip.png");
+                break;
+            case "reward-status-wrapper":
+                $("#imgLogo").attr("src", "../navigation-layout/assets/logo-gold.png");
+                break;
+            case "account-settings-wrapper":
+                $("#imgLogo").attr("src", "../navigation-layout/assets/logo-elite.png");
+                break;
+            case "payment-methods-wrapper":
+                $("#imgLogo").attr("src", "../navigation-layout/assets/logo-member.png");
+        }
     })
+    $("#orderDetailsBtn").on("click", function(){
+        contentElements.addClass("hidden");
+        contentElements.filter(".order-details-wrapper").removeClass("hidden");
+        $(".boundary-menu").removeClass("hidden");
+    })
+
 })
